6ea08db8f3220946c2fa320ecce468f3ac2b2268
Sameer Alhaddad <sameeralhaddad38@gmail.com>
2025-08-27 22:44:15 +0200

    auth implemented

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,39 @@
+import { Inngest } from "inngest";
+import { User } from "../models/user.model";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "slack-clone" });
+
+const syncUser = inngest.createFunction(
+	{ id: "sync-user" },
+	{ event: "clerk/user.created" },
+	async ({ event }) => {
+		await connectDB();
+
+		const { id, email_addresses, first_name, last_name, image_url } =
+			event.data;
+
+		const newUser = {
+			email: email_addresses[0]?.email_address,
+			name: `${first_name || ""} ${last_name || ""}`,
+			image: image_url,
+		};
+
+		await User.create(newUser);
+
+		// TODO: You can also handle errors and edge cases as needed
+	}
+);
+
+const deleteUserFromDB = inngest.createFunction(
+	{ id: "delete-user-from-db" },
+	{ event: "clerk/user.deleted" },
+	async ({ event }) => {
+		const { id } = event.data;
+		await User.deleteOne({ clerkId: id });
+		// ToDo: do more things here
+	}
+);
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUser, deleteUserFromDB];
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		email: { type: String, required: true, unique: true },
+		name: { type: String, required: true },
+		image: { type: String, required: true },
+		clerkId: { type: String, required: true, unique: true },
+	},
+	{ timestamps: true }
+); // Automatically manage createdAt and updatedAt fields
+
+export const User = mongoose.model("User", userSchema);
