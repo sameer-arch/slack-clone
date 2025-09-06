3515b29647cafadbe85caa1319fe1f16abfe3ddb
Sameer Alhaddad <sameeralhaddad38@gmail.com>
2025-09-06 12:23:25 +0200

    stream & sentry added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,27 @@ import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { inngest, functions } from "./config/inngest.js";
 import { serve } from "inngest/express";
+import chatRoutes from "./routes/chat.route.js";
+
+import * as Sentry from "@sentry/node";
 
 const app = express();
 
 app.use(express.json());
 app.use(clerkMiddleware()); // req.auth will be available in the request object
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.get("/debug-sentry", (req, res) => {
+	throw new Error("Sentry is configured correctly! This is an error message");
+});
 
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+
+Sentry.setupExpressErrorHandler(app);
 
 const startServer = async () => {
 	try {
@@ -22,7 +32,7 @@ const startServer = async () => {
 		if (env.NODE_ENV !== "production") {
 			app.listen(env.PORT, () => {
 				console.log("Server is running on http://localhost:" + env.PORT);
-				connectDB();
+				// connectDB();
 			});
 		}
 	} catch (error) {
